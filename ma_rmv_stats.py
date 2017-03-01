d05daee1a0fd34069f72b56cfd3b31b95b2b49a1
Patrick Eaton <preaton@google.com>
2017-03-01 13:28:19 -0500

    Add comments for set-up on a GCE instance.

--- ma_rmv_stats.py
+++ ma_rmv_stats.py
@@ -12,8 +12,25 @@ import strict_rfc3339
 #
 # Query the MA RMV for branch wait times and post as custom metrics.
 #
-# Run this via cron with the follow crontab entry:
-#   2,7,12,17,22,27,32,37,42,47,52,57 * * * * python /full/path/to/ma_rmv_metric_gen.py > /var/log/rmv.log
+# To run this script on a GCP instance:
+#   Create a new GCP instance. If you want the test query--used to verify that
+#   the script is succeeding--to work, make sure the include the proper scope.
+#   It requires the "Stackdriver Monitoring API" to have "Full" scope.
+#
+# $ sudo apt-get install git python-pip
+# $ git clone https://github.com/premoxios/ma-rmv-stats.git
+# $ cd ma-rmv-stats
+# $ sudo pip install --upgrade pip virtualenv
+# $ virtualenv virtenv
+# $ source virtenv/bin/activate
+# $ pip install -r requirements.txt
+# $ python ma-rmv-stats.py --project_id [PROJECT_ID]
+#
+# To collect data on a regular schedule via cron, add a crontab entry as, e.g.,
+# $ sudo sh -c 'echo "2,7,12,17,22,27,32,37,42,47,52,57 * * * * root \
+# $  source /[PATH]/ma-rmv-stats/virtenv/bin/activate && \
+# $  python /[PATH]/ma-rmv-stats/ma_rmv_stats.py --project_id [PROJECT_ID] >> \
+# $  /var/log/rmv.log" >> /etc/crontab'
 #
 
 MA_RMV_WAIT_TIME_CUSTOM_METRIC_TYPE = "custom.googleapis.com/ma_rmv_wait_time"
